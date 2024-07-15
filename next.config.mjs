1c68d159cc14578a9dde040c3a9e0f707cb4f63e
Eun Oh <bdohhhhh@gmail.com>
2024-07-15 22:35:58 +0900

    240715 [add] : pwa 기본 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+    dest: "public",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
