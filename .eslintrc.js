24babb235ec2c92510ebdb1d3567daf83290a5a3
Eun Oh <bdohhhhh@gmail.com>
2024-07-20 22:03:37 +0900

    [script] : chore eslint-plugin-prettier eslint-config-prettier 의존성 설치 및 .eslintrc.js 기본설정

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    extends: ["next/core-web-vitals", "plugin:prettier/recommended"],
+    // plugins: ['prettier', 'unused-imports'],
+    plugins: ["prettier"],
+    rules: {
+        // 선언되지 않은 변수 또는 임포트 구문 정리 규칙
+        "no-undef": "error",
+        // 'unused-imports/no-unused-imports': 'error',
+
+        // 프리티어 설정
+        "prettier/prettier": [
+            "error",
+            {
+                singleQuote: true,
+                semi: true,
+                useTabs: true,
+                tabWidth: 4,
+                trailingComma: "all",
+                printWidth: 80,
+                bracketSpacing: true,
+                arrowParens: "avoid",
+            },
+        ],
+    },
+};
