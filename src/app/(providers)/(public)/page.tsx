3c6cf67ed3a4d9958c689e9e443a4a9904efb66e
Eun Oh <bdohhhhh@gmail.com>
2024-07-20 13:08:49 +0900

    [update] : (public)/page.tsx 생성 및 app/page.tsx 삭제

--- src/app/(providers)/(public)/page.tsx
+++ src/app/(providers)/(public)/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
+function HomePage() {
     return (
         <div className="text-center">
             <h1>Home Page</h1>
         </div>
     );
 }
+
+export default HomePage;
