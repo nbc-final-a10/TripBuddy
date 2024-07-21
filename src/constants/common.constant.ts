6b2bad352752661f386b75fb49d39fe8932c06ac
Eun Oh <bdohhhhh@gmail.com>
2024-07-21 10:51:35 +0900

    [chore] : auth 의 모든 파일 eslint+prettier 설정에 맞도록 수정(누락된파일들도 수정)

--- src/constants/common.constant.ts
+++ src/constants/common.constant.ts
@@ -1,1 +1,2 @@
-export const PUBLIC_URL = process.env.NEXT_PUBLIC_BASE_URL || process.env.NEXT_PUBLIC_BASE_URL_SECURE;
+export const PUBLIC_URL =
+    process.env.NEXT_PUBLIC_BASE_URL || process.env.NEXT_PUBLIC_BASE_URL_SECURE;
