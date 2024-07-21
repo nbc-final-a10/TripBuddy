69cde48f03267ae52b13f87c94fae99596df4bfa
Eun Oh <bdohhhhh@gmail.com>
2024-07-21 18:46:36 +0900

    [chore] : api-services, constants, contexts 폴더 생성 및 route group 기본 설정, next 이미지 외부 허용 주소 설정, tailwind 컬러 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,31 @@
-import withPWAInit from "next-pwa";
+import withPWAInit from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'pedixhwyfardtsanotrp.supabase.co',
+            },
+            {
+                protocol: 'http',
+                hostname: 'k.kakaocdn.net',
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            },
+        ],
+    },
 };
 
 const withPWA = withPWAInit({
-  dest: "public",
-  disable: process.env.NODE_ENV === 'development',
-  mode: 'production',
-  buildExcludes: [/middleware-manifest\.json$/],
-  swSrc: 'public/sw.js',
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    mode: 'production',
+    buildExcludes: [/middleware-manifest\.json$/],
+    swSrc: 'public/sw.js',
 });
 
 export default withPWA(nextConfig);
