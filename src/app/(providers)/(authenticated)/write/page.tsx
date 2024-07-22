58bedfd1ec03f5bc67648c30137d6c52aac770b9
Captain-Kim <brighthero@kakao.com>
2024-07-22 15:35:17 +0900

    merged dev

--- src/app/(providers)/(authenticated)/write/page.tsx
+++ src/app/(providers)/(authenticated)/write/page.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-export default function WritePage() {
-    return <div>WritePage</div>;
-}
+const WritePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Write Page</h1>
+            <p>This is the write page.</p>
+        </div>
+    );
+};
+
+export default WritePage;
