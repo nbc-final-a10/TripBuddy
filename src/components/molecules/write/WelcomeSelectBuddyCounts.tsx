dd48badba42f6443f75daa530c806c8ffb8ae014
Captain-Kim <brighthero@kakao.com>
2024-07-22 18:20:28 +0900

    style: 버디즈 선택 컴포넌트 간 마진 세부 조정

--- src/components/molecules/write/WelcomeSelectBuddyCounts.tsx
+++ src/components/molecules/write/WelcomeSelectBuddyCounts.tsx
@@ -5,15 +5,15 @@ export default function WelcomeSelectBuddyCounts() {
 
     return (
         <div className="flex flex-col items-center">
-            <div className="flex mt-10 mb-5 text-2xl xl:text-4xl xl:mt-20 xl:mb-20">
+            <div className="flex mt-10 mb-2 text-2xl xl:text-4xl xl:mt-20 xl:mb-5">
                 여정 인원을 선택해주세요
             </div>
-            <div className="flex flex-col items-center text-gray-600 xl:text-lg xl:mb-10">
+            <div className="flex flex-col items-center text-gray-600 text-sm xl:text-lg mb-5 xl:mb-10">
                 <p>최대 여정 인원은 4명까지 선택 가능해요.</p>
             </div>
             <div className="flex flex-row gap-[2px]">
                 <button
-                    className="text-[#647484] w-[50px] h-[50px] bg-[#edeff1] hover:bg-gray-400 flex items-center justify-center"
+                    className="text-[#647484] w-[30px] h-[30px] xl:w-[50px] xl:h-[50px] bg-[#edeff1] hover:bg-gray-400 flex items-center justify-center"
                     onClick={() => {
                         if (count > 1) {
                             setCount(count - 1);
@@ -29,12 +29,12 @@ export default function WelcomeSelectBuddyCounts() {
                     readOnly
                     name="custom-input-number"
                 />
-                <div className="bg-[#edeff1] w-[50px] h-[50px] text-[#647484] md:text-base flex items-center justify-center cursor-default">
+                <div className="bg-[#edeff1] w-[30px] h-[30px] xl:w-[50px] xl:h-[50px] text-[#647484] md:text-base flex items-center justify-center cursor-default">
                     <span>{count}</span>
                 </div>
                 <div className="buttons-wrap flex flex-col items-center gap-[2px]">
                     <button
-                        className="text-[#647484] w-[50px] h-[50px] bg-[#edeff1] hover:bg-gray-400 flex items-center justify-center"
+                        className="text-[#647484] w-[30px] h-[30px] xl:w-[50px] xl:h-[50px] bg-[#edeff1] hover:bg-gray-400 flex items-center justify-center"
                         onClick={() => {
                             if (count < 5) {
                                 setCount(count + 1);
