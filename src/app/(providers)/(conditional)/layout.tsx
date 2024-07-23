e555549463e438ac3c54fe0b20abaa347641ceec
Eun Oh <bdohhhhh@gmail.com>
2024-07-23 13:07:57 +0900

    [update] : pwa 관련 빌드 문제 해결 중 - build error Fix 시도 3

---
+++ src/app/(providers)/(conditional)/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ConditionalLayout: React.FC<{ children: React.ReactNode }> = ({
+    children,
+}) => {
+    return <>{children}</>;
+};
+
+export default ConditionalLayout;
