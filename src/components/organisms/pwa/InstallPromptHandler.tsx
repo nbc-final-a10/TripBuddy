937ab200f2c595f68927278f06bf94448bd30754
Eun Oh <bdohhhhh@gmail.com>
2024-07-23 12:59:06 +0900

    [update] : pwa 관련 빌드 문제 해결을 위해 dev merge

--- src/components/organisms/pwa/InstallPromptHandler.tsx
+++ src/components/organisms/pwa/InstallPromptHandler.tsx
@@ -1,29 +1,26 @@
 'use client';
 
-// Todo: PWA가 이미 설치된 경우 버튼 동작 안 함 => 사용자에게 설치 여부 alert 알림 주기
-// Todo: 모바일 브라우저(사파리, 크롬)에서 PWA 설치 버튼 안 먹히는 증상 해결
-
 import useCheckPwa from '@/hooks/useCheckPwa';
 import { useEffect, useState } from 'react';
+
+// TypeScript 인터페이스 정의
+interface BeforeInstallPromptEvent extends Event {
+    readonly platforms: string[];
+    readonly userChoice: Promise<{
+        outcome: 'accepted' | 'dismissed';
+        platform: string;
+    }>;
+    prompt(): Promise<void>;
+}
 
 const InstallPromptHandler = () => {
     const [deferredPrompt, setDeferredPrompt] = useState<Event | null>(null);
     const isPwa = useCheckPwa();
 
-    useEffect(() => {
-        const handler = (e: Event) => {
-            e.preventDefault();
-            setDeferredPrompt(e);
-        };
-
-        window.addEventListener('beforeinstallprompt', handler as any);
-
-        return () => {
-            window.removeEventListener('beforeinstallprompt', handler as any);
-        };
-    }, []);
+    console.log(deferredPrompt);
 
     const handleInstallClick = () => {
+        console.log('버튼 클릭되었음');
         if (deferredPrompt) {
             (deferredPrompt as any).prompt();
             (deferredPrompt as any).userChoice.then((choiceResult: any) => {
@@ -37,22 +34,46 @@ const InstallPromptHandler = () => {
         }
     };
 
+    useEffect(() => {
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('/sw.js', { type: 'module' }).then(
+                function (registration) {
+                    console.log(
+                        'Service Worker registered with scope:',
+                        registration.scope,
+                    );
+                },
+                function (error) {
+                    console.log('Service Worker registration failed:', error);
+                },
+            );
+        }
+
+        const handler = (e: BeforeInstallPromptEvent) => {
+            console.log('beforeinstallprompt 이벤트 발생', e);
+            e.preventDefault();
+            setDeferredPrompt(e);
+        };
+
+        window.addEventListener('beforeinstallprompt', handler as any);
+
+        return () => {
+            window.removeEventListener('beforeinstallprompt', handler as any);
+        };
+    }, []);
+
     if (isPwa) {
         return null;
     }
 
-    if (!isPwa) {
-        return (
-            <>
-                <button
-                    onClick={handleInstallClick}
-                    className="bg-blue-500 text-white px-4 py-2 rounded-md"
-                >
-                    홈 화면에 추가하기
-                </button>
-            </>
-        );
-    }
+    return (
+        <button
+            onClick={handleInstallClick}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md"
+        >
+            홈 화면에 추가하기
+        </button>
+    );
 };
 
 export default InstallPromptHandler;
