f14723f1bfd3ca01f2945bcc6a4879b9ed876e22
Eun Oh <bdohhhhh@gmail.com>
2024-07-24 23:39:42 +0900

    [update]: queryParams 의 값만 가져오기 위해서 middleware.ts 에서 funnel=어쩌구 일때, 어쩌구를 x-funnel 에 담는 로직 추가

--- src/utils/common/getPathnameServer.ts
+++ src/utils/common/getPathnameServer.ts
@@ -2,5 +2,8 @@ import { headers } from 'next/headers';
 
 export const getPathnameServer = () => {
     const headersList = headers();
-    return headersList.get('x-pathname');
+    const pathname = headersList.get('x-pathname');
+    const queryParams = headersList.get('x-funnel');
+
+    return { pathname, queryParams };
 };
