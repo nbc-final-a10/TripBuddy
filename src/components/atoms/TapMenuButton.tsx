127e5aae08f23098d99d9f5f9150d015ee875498
slsl2 <kimminji7153@gmail.com>
2024-07-24 13:50:41 +0900

    refactor: 탭메뉴 버튼 컴포넌트 분리 및 Link 추가

--- src/components/atoms/TapMenuButton.tsx
+++ src/components/atoms/TapMenuButton.tsx
@@ -1,32 +1,27 @@
-import Image from 'next/image';
+'use client';
 
-const TapMenu = () => {
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+interface TapMenuButtonProps {
+    iconName: string;
+    href: string;
+}
+
+const TapMenuButton: React.FC<TapMenuButtonProps> = ({ iconName, href }) => {
     return (
-        <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[375px] bg-white border-t-2 border-gray-200 grid grid-cols-4 xl:hidden">
-            <button className="flex items-center justify-center p-4 hover:bg-gray-200 focus:outline-none">
-                <Image src="/svg/Home.svg" alt="Home" width={24} height={24} />
-            </button>
+        <Link href={href}>
             <button className="flex items-center justify-center p-4 hover:bg-gray-200 focus:outline-none">
                 <Image
-                    src="/svg/Travel.svg"
-                    alt="Travel"
+                    src={`/svg/${iconName}.svg`}
+                    alt={iconName}
                     width={24}
                     height={24}
                 />
             </button>
-            <button className="flex items-center justify-center p-4 hover:bg-gray-200 focus:outline-none">
-                <Image src="/svg/Chat.svg" alt="Chat" width={24} height={24} />
-            </button>
-            <button className="flex items-center justify-center p-4 hover:bg-gray-200 focus:outline-none">
-                <Image
-                    src="/svg/Mypage.svg"
-                    alt="MyPage"
-                    width={24}
-                    height={24}
-                />
-            </button>
-        </div>
+        </Link>
     );
 };
 
-export default TapMenu;
+export default TapMenuButton;
--- src/components/molecules/TapMenu.tsx
+++ src/components/molecules/TapMenu.tsx
@@ -1,30 +1,14 @@
-import Image from 'next/image';
+import React from 'react';
+import TapMenuButton from '../atoms/TapMenuButton';
 
-const TapMenu = () => {
+const TapMenu: React.FC = () => {
     return (
-        <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[375px] bg-white border-t-2 border-gray-200 grid grid-cols-4 xl:hidden">
-            <button className="flex items-center justify-center p-4 hover:bg-gray-200 focus:outline-none">
-                <Image src="/svg/Home.svg" alt="Home" width={24} height={24} />
-            </button>
-            <button className="flex items-center justify-center p-4 hover:bg-gray-200 focus:outline-none">
-                <Image
-                    src="/svg/Travel.svg"
-                    alt="Travel"
-                    width={24}
-                    height={24}
-                />
-            </button>
-            <button className="flex items-center justify-center p-4 hover:bg-gray-200 focus:outline-none">
-                <Image src="/svg/Chat.svg" alt="Chat" width={24} height={24} />
-            </button>
-            <button className="flex items-center justify-center p-4 hover:bg-gray-200 focus:outline-none">
-                <Image
-                    src="/svg/Mypage.svg"
-                    alt="MyPage"
-                    width={24}
-                    height={24}
-                />
-            </button>
+        <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[375px] bg-white border-t-2 border-gray-200 grid grid-cols-4 justify-items-center xl:hidden">
+            <TapMenuButton iconName="Home" href="/" />
+            <TapMenuButton iconName="Trip" href="/" />
+            <TapMenuButton iconName="Chat" href="/chat" />
+            <TapMenuButton iconName="Mypage" href="/" />
+            {/* 주소 변경 각 페이지 담당자가 해주시면 됩니다 */}
         </div>
     );
 };
