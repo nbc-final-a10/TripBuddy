485659b8fc57d928074d7737838ae6e64f1aef38
Eun Oh <bdohhhhh@gmail.com>
2024-07-24 16:54:52 +0900

    [style] : dev를 머지하여 헤더, 하단바가 생겼으므로 그에 맞춰 인증/인가 페이지 높이 및 배치  수정

--- src/components/molecules/auth/AuthPageWrapper.tsx
+++ src/components/molecules/auth/AuthPageWrapper.tsx
@@ -2,8 +2,8 @@ import { PropsWithChildren } from 'react';
 
 function AuthPageWrapper({ children }: PropsWithChildren) {
     return (
-        <div className="flex justify-center items-center min-h-dvh bg-gray-50 max-w-full mx-auto">
-            <section className="h-dvh w-full flex flex-col items-center justify-center my-0 mx-auto bg-white">
+        <div className="flex fixed justify-center items-center bg-gray-50 max-w-full mx-auto top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <section className="h-full w-full flex flex-col items-center justify-center my-0 mx-auto bg-white">
                 <div className="h-full w-full flex flex-col items-center justify-center gap-10">
                     {children}
                 </div>
