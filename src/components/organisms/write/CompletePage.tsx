238b400eedde72af4a62646ebaf4a54ac8aadab5
Captain-Kim <brighthero@kakao.com>
2024-07-25 12:38:49 +0900

    feat: add FunnelPattern 기본 시퀀스 구성 (웰컴~완료 컴포넌트까지)

---
+++ src/components/organisms/write/CompletePage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CompletePage() {
+    return <div>CompletePage</div>;
+}
---
+++ src/components/organisms/write/WriteTrip.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function WriteTrip() {
+    return <div>WriteTrip</div>;
+}
