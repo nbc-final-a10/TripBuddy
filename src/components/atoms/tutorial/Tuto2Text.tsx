3044aba34fdcd59031767d514e807d47e4865a3a
unknown <kyara0324@naver.com>
2024-07-25 15:02:05 +0900

    chore: tutorial 폴더구조 확인요청

--- src/components/atoms/tutorial/Tuto2Text.tsx
+++ src/components/atoms/tutorial/Tuto2Text.tsx
@@ -1,8 +1,9 @@
 const Tuto2Text = () => {
     return (
         <h2 className="text-lg font-semibold mb-4 text-center">
-            점점 쌓이는 포인트로 나만의 사무실을 꾸미고 승진의 기회까지
-            잡으세요!
+            <p>점점 쌓이는 포인트로</p>
+            <p>나만의 사무실을 꾸미고</p>
+            <p>승진의 기회까지잡으세요!</p>
         </h2>
     );
 };
