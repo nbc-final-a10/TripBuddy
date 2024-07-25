cdf57533063e778906e1e96bff348e611e1a6700
Eun Oh <bdohhhhh@gmail.com>
2024-07-25 22:36:42 +0900

    [update]: location 타입 추론 더 정확하게 수정

--- src/types/Location.types.ts
+++ src/types/Location.types.ts
@@ -1,8 +1,22 @@
 import { locationData } from '@/data/location';
 
-export type Location = {
+type LocationName = {
+    ko: string;
+    en: string;
+};
+
+type SubLocation = {
     name: string;
-    subLocations?: Location[];
+};
+
+type NestedSubLocation = {
+    name: LocationName;
+    subLocations: SubLocation[];
+};
+
+type LocationData = {
+    name: LocationName;
+    subLocations: (NestedSubLocation | SubLocation)[];
 };
 
 // Todo : 이 부분 추가로 공부할 것
@@ -16,3 +30,33 @@ type ExtractSecondLevelNames<T extends { subLocations?: readonly any[] }> =
 export type SecondLevelNames = ExtractSecondLevelNames<
     (typeof locationData)[number]
 >;
+
+type LocationDataType = typeof locationData;
+type Location =
+    LocationDataType[number]['subLocations'][number]['subLocations'][number];
+
+// type ExtractThirdLevelNames<T extends { subLocations?: readonly any[] }> =
+//     T['subLocations'] extends readonly (infer U)[]
+//         ? U extends { subLocations: readonly (infer V)[] }
+//             ? V extends { name: infer W }
+//                 ? W
+//                 : never
+//             : never
+//         : never;
+
+// export type ThirdLevelNames = ExtractThirdLevelNames<
+//     (typeof locationData)[number]['subLocations'][number]
+// >;
+
+// type ExtractThirdLevelNames<T extends { subLocations?: readonly any[] }> =
+//     T['subLocations'] extends readonly (infer U)[]
+//         ? U extends { subLocations: readonly (infer V)[] }
+//             ? V extends { name: { ko: infer W } }
+//                 ? W
+//                 : never
+//             : never
+//         : never;
+
+// export type ThirdLevelNames = ExtractThirdLevelNames<
+//     (typeof locationData)[number]['subLocations'][number]
+// >;
