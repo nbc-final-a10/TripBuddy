23f9b770950ec63c9f4faaeca2977b763a22f3f3
Eun Oh <bdohhhhh@gmail.com>
2024-07-25 22:44:56 +0900

    [update]: location 타입 3번째 레벨 까지 더 정확하게 리터럴 유니온 타입으로 수정

--- src/types/Location.types.ts
+++ src/types/Location.types.ts
@@ -32,31 +32,6 @@ export type SecondLevelNames = ExtractSecondLevelNames<
 >;
 
 type LocationDataType = typeof locationData;
-type Location =
-    LocationDataType[number]['subLocations'][number]['subLocations'][number];
 
-// type ExtractThirdLevelNames<T extends { subLocations?: readonly any[] }> =
-//     T['subLocations'] extends readonly (infer U)[]
-//         ? U extends { subLocations: readonly (infer V)[] }
-//             ? V extends { name: infer W }
-//                 ? W
-//                 : never
-//             : never
-//         : never;
-
-// export type ThirdLevelNames = ExtractThirdLevelNames<
-//     (typeof locationData)[number]['subLocations'][number]
-// >;
-
-// type ExtractThirdLevelNames<T extends { subLocations?: readonly any[] }> =
-//     T['subLocations'] extends readonly (infer U)[]
-//         ? U extends { subLocations: readonly (infer V)[] }
-//             ? V extends { name: { ko: infer W } }
-//                 ? W
-//                 : never
-//             : never
-//         : never;
-
-// export type ThirdLevelNames = ExtractThirdLevelNames<
-//     (typeof locationData)[number]['subLocations'][number]
-// >;
+export type ThirdLevelNames =
+    LocationDataType[number]['subLocations'][number]['subLocations'][number]['name'];
