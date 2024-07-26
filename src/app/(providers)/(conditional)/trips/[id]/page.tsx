803b7cc2d262b5b15c1e2d978446b2d5d25f1592
Eun Oh <bdohhhhh@gmail.com>
2024-07-26 12:44:45 +0900

    [update]: 여정 디테일 페이지 기본 스타일링 진행중 1

--- src/app/(providers)/(conditional)/trips/[id]/page.tsx
+++ src/app/(providers)/(conditional)/trips/[id]/page.tsx
@@ -1,7 +1,20 @@
+import TripCard from '@/components/molecules/trips/TripCard';
 import React from 'react';
 
 const TripsDetailPage: React.FC = () => {
-    return <div>TripsDetailPage</div>;
+    return (
+        <div>
+            <div></div>
+            <TripCard
+                title={`Trip 123123`}
+                description={`Description 123123`}
+                date="2022-01-01"
+                location="Seoul"
+                participants="10"
+                mode="detail"
+            />
+        </div>
+    );
 };
 
 export default TripsDetailPage;
--- src/components/molecules/trips/TripCard.tsx
+++ src/components/molecules/trips/TripCard.tsx
@@ -1,14 +1,11 @@
 'use client';
 
-import TripChip from '@/components/atoms/trips/TripChip';
 import React from 'react';
 import Calendar_month from '../../../../public/svg/Calendar_month.svg';
 import Distance from '../../../../public/svg/Distance.svg';
 import Groups from '../../../../public/svg/Groups.svg';
 import Chip from '@/components/atoms/common/O_Chip';
-import { VariantProps } from 'class-variance-authority';
-
-type ChipVariantsType = VariantProps<typeof TripCardVariants>;
+import clsx from 'clsx';
 
 type TripCardProps = {
     title: string;
@@ -16,18 +13,29 @@ type TripCardProps = {
     date: string;
     location: string;
     participants: string;
-} & ChipVariantsType &
-    React.ComponentProps<'div'>;
+    mode?: 'card' | 'detail';
+};
 
 const TripCard: React.FC<TripCardProps> = ({
     title,
     date,
     location,
     participants,
+    mode = 'card',
 }) => {
     return (
-        <div className="bg-white rounded-lg shadow-md max-h-[230px] box-border">
-            <div className="bg-gray-200 p-4 rounded-lg rounded-b-none box-border h-[80%]">
+        <div
+            className={clsx(
+                'bg-white rounded-lg max-h-[230px] box-border',
+                mode === 'detail' ? '' : 'cursor-pointer shadow-md',
+            )}
+        >
+            <div
+                className={clsx(
+                    'bg-gray-200 p-4 rounded-lg rounded-b-none box-border h-[80%]',
+                    mode === 'detail' ? 'bg-white rounded-none' : '',
+                )}
+            >
                 <div className="flex flex-col">
                     <div className="flex flex-col gap-2 box-border pb-4">
                         <h3 className="text-lg font-bold">{title}</h3>
@@ -61,11 +69,32 @@ const TripCard: React.FC<TripCardProps> = ({
                 </div>
             </div>
 
-            <div className="flex justify-between w-full text-white rounded-lg h-[20%]">
-                <button className="bg-gray-400 w-1/2 p-2 rounded-t-none rounded-br-none rounded-bl-lg">
+            <div
+                className={clsx(
+                    'flex w-full text-white rounded-lg h-[20%]',
+                    mode === 'detail'
+                        ? 'bg-white text-gray-950 rounded-none justify-center gap-4'
+                        : 'justify-between',
+                )}
+            >
+                <button
+                    className={clsx(
+                        'bg-gray-400 p-2',
+                        mode === 'detail'
+                            ? 'bg-white text-gray-950 rounded-md border border-gray-500 w-[40%]'
+                            : 'rounded-t-none rounded-br-none rounded-bl-lg w-1/2',
+                    )}
+                >
                     찜하기
                 </button>
-                <button className="bg-gray-500 w-1/2 p-2 rounded-t-none rounded-bl-none rounded-br-lg">
+                <button
+                    className={clsx(
+                        'bg-gray-500 w-1/2 p-2',
+                        mode === 'detail'
+                            ? 'bg-white text-gray-950 rounded-md border border-gray-500 w-[40%]'
+                            : 'rounded-t-none rounded-bl-none rounded-br-lg w-1/2',
+                    )}
+                >
                     채팅하기
                 </button>
             </div>
