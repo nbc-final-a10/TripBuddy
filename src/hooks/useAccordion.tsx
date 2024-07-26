fabb2a65ee3e34cf50f4d99d989943f77e4d97fb
Captain-Kim <brighthero@kakao.com>
2024-07-27 04:02:16 +0900

    feat: add useAccordion 커스텀 훅 작성

---
+++ src/hooks/useAccordion.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+type AccordionProps = {
+    title: string;
+    isOpen: boolean;
+    toggleAccordion: () => void;
+    children: React.ReactNode;
+};
+
+export function useAccordion(initialOpen = false) {
+    const [isOpen, setIsOpen] = useState(initialOpen);
+
+    const toggleAccordion = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return {
+        isOpen,
+        toggleAccordion,
+    };
+}
+const Accordion: React.FC<AccordionProps> = ({
+    title,
+    isOpen,
+    toggleAccordion,
+    children,
+}) => {
+    return (
+        <div className="py-5 border-b border-gray-200 mx-4">
+            <details className="group" open={isOpen}>
+                <summary
+                    className="flex justify-between items-center font-medium cursor-pointer list-none"
+                    onClick={toggleAccordion}
+                >
+                    <span className="text-center xl:text-xl">{title}</span>
+                    <span className="transition group-open:rotate-180 ml-auto">
+                        <svg
+                            fill="none"
+                            height="24"
+                            shapeRendering="geometricPrecision"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="1.5"
+                            viewBox="0 0 24 24"
+                            width="24"
+                        >
+                            <path d="M6 9l6 6 6-6"></path>
+                        </svg>
+                    </span>
+                </summary>
+                <div className="text-neutral-600 mt-3 group-open:animate-fadeIn">
+                    {children}
+                </div>
+            </details>
+        </div>
+    );
+};
+
+export default Accordion;
