061de364f3dd1c04f12f169e688f47a13fcee752
Captain-Kim <brighthero@kakao.com>
2024-07-27 01:22:08 +0900

    fix: 웰컴페이지 페이지 원형 이미지 깨짐 현상 해결

--- src/components/molecules/write/WelcomeImage.tsx
+++ src/components/molecules/write/WelcomeImage.tsx
@@ -8,7 +8,7 @@ export default function WelcomeImage() {
             alt="buddy character"
             width={250}
             height={250}
-            className="rounded-full object-contain xl:w-[500px] xl:h-[500px]"
+            className="rounded-full h-[250px] w-[250px] xl:w-[500px] xl:h-[500px]"
         />
     );
 }
