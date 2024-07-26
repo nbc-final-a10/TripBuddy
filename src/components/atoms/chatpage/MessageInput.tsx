8e26a5897074112dee5b6798ecd10bba0e46c3cc
slsl2 <kimminji7153@gmail.com>
2024-07-26 17:21:48 +0900

    채팅 페이지 MessageInput 컴포넌트 분리

--- src/components/atoms/chatpage/MessageInput.tsx
+++ src/components/atoms/chatpage/MessageInput.tsx
@@ -1,5 +1,21 @@
+import Image from 'next/image';
+
 const MessageInput = () => {
-    return;
+    return (
+        <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[375px] bg-white border-gray-200 flex justify-around items-center p-2">
+            <button className="w-[28px] h-[28px] flex items-center justify-center">
+                <Image src="/svg/Plus.svg" alt="Plus" width="15" height="15" />
+            </button>
+            <input
+                type="text"
+                placeholder="메시지 작성 ..."
+                className="outline-none w-[291px] h-[37px] bg-gray-100 rounded-[8px] text-xs p-2"
+            ></input>
+            <button className="w-[28px] h-[28px] flex items-center justify-center">
+                <Image src="/svg/Send.svg" alt="Send" width="15" height="15" />
+            </button>
+        </div>
+    );
 };
 
 export default MessageInput;
