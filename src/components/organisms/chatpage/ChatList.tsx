a3b428d4ca3c5086662dc3c1f6f4287750a62200
slsl2 <kimminji7153@gmail.com>
2024-07-30 19:28:21 +0900

    chore: chat list 더미데이터 타이틀만 수정

--- src/components/organisms/chatpage/ChatList.tsx
+++ src/components/organisms/chatpage/ChatList.tsx
@@ -4,13 +4,13 @@ import React from 'react';
 const chatData = [
     {
         contractTripId: '8a9313c8-0c0a-4ba4-804d-b0e101f39d9b',
-        chatName: '부산 여행 카페투어 같이 해요!',
-        lastMessage: '네 좋습니다 그때 거기서 뵙는 거로 하죠!',
+        chatName: '메시지 테스트!',
+        lastMessage: 'last message',
     },
     {
         contractTripId: 'c8d9a501-5d13-4fb5-9e01-86cf98905d28',
-        chatName: '부산 여행 카페투어 같이 해요!',
-        lastMessage: '네 좋습니다 그때 거기서 뵙는 거로 하죠!',
+        chatName: '메시지 리스트 테스트',
+        lastMessage: 'last message',
     },
 ];
 
