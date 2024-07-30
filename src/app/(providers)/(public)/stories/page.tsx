e43f5c173e696361eed8154646064fbb486ec347
Eun Oh <bdohhhhh@gmail.com>
2024-07-31 00:19:33 +0900

    [update]: StoryCard 프랍받게 수정 카드 UI 완성

--- src/app/(providers)/(public)/stories/page.tsx
+++ src/app/(providers)/(public)/stories/page.tsx
@@ -1,11 +1,18 @@
-import StoryCard from '@/components/molecules/story/StoryCard';
+import StoryCard from '@/components/molecules/stories/StoryCard';
 import React from 'react';
 
 const StoriesPage: React.FC = () => {
     return (
         <section className="flex flex-col gap-4">
             {Array.from({ length: 10 }, (_, index) => (
-                <StoryCard key={index} />
+                <StoryCard
+                    key={index}
+                    name="김소희"
+                    created_at="2024-07-26"
+                    profile_image="/images/test.webp"
+                    background_image="/images/test2.webp"
+                    mode="my"
+                />
             ))}
         </section>
     );
