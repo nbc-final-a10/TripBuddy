c7647a06f5f5df2e025a44d13fa4f48e01f10aa7
Captain-Kim <brighthero@kakao.com>
2024-07-31 18:20:09 +0900

    feat: add home page 추천 인기 버디즈 목록 스켈레톤 렌더링 추가

--- src/components/organisms/homepage/HomePageRecommnedBuddiesList.tsx
+++ src/components/organisms/homepage/HomePageRecommnedBuddiesList.tsx
@@ -1,12 +1,23 @@
 import MascotImage from '@/components/atoms/common/O_MascotImage';
 import BuddyTemperature from '@/components/atoms/profile/BuddyTemperature';
+import SkeletonLoader from '@/components/atoms/profile/SkeletonLoader';
 import useTapScroll from '@/hooks/useTapScroll';
 import React, { useState, useEffect, useRef } from 'react';
+
+type Buddy = {
+    buddy_nickname: string;
+    buddy_birth: number;
+    buddy_temperature: number;
+    buddy_preferred_theme1: string;
+    buddy_preferred_theme2: string;
+    buddy_preferred_theme3: string;
+};
 
 function HomePageRecommnedBuddiesList() {
     const buddiesRef = useRef<HTMLDivElement>(null);
     const { createMouseDownHandler } = useTapScroll();
     const [loading, setLoading] = useState(true);
+    const [buddies, setBuddies] = useState<Buddy[] | null>(null);
 
     useEffect(() => {
         const fetchBuddies = async () => {
@@ -16,6 +27,7 @@ function HomePageRecommnedBuddiesList() {
                 );
                 const data = await response.json();
                 console.log(data);
+                setBuddies(data.buddies);
                 setLoading(false);
             } catch (error) {
                 console.error('버디 추천 리스트 통신 오류 발생:', error);
@@ -27,59 +39,61 @@ function HomePageRecommnedBuddiesList() {
     return (
         <div className="container">
             {loading ? (
-                <div className="box">
-                    <div className="skeleton">
-                        <div className="skeleton-left flex1">
-                            <div className="square"></div>
-                        </div>
-                        <div className="skeleton-right flex2">
-                            <div className="line h17 w40 m10"></div>
-                            <div className="line"></div>
-                            <div className="line h8 w50"></div>
-                            <div className="line w75"></div>
-                        </div>
-                    </div>
+                <div className="flex space-x-4 scrollbar-hide overflow-x-auto">
+                    {Array.from({ length: 5 }).map((_, index) => (
+                        <SkeletonLoader key={index} />
+                    ))}
                 </div>
             ) : (
-                // 로딩완료 이후
                 <div>
-                    {/* Todo: PC에서는 스크롤바 보이게 하기 */}
                     <div
                         className="flex overflow-x-auto scrollbar-hide xl:scrollbar-default"
                         onMouseDown={createMouseDownHandler(buddiesRef)}
                     >
-                        {Array.from({ length: 10 }).map((_, index) => (
-                            <div
-                                key={index}
-                                className="min-w-[200px] h-[75px] mx-1 rounded border border-gray-200 shadow-md"
-                            >
-                                <div className="flex items-center w-[120px] h-full">
-                                    <div className="flex-shrink-0 w-[75px] h-[75px]">
-                                        <MascotImage intent="happy" />
-                                    </div>
-                                    <div className="ml-2 flex flex-col">
-                                        <span className="text-xs font-bold text-gray-500 whitespace-nowrap">
-                                            #감성 #즉흥적
-                                        </span>
-                                        <div className="text-m font-bold whitespace-nowrap">
-                                            <span>장원영</span>
-                                            <span> / </span>
-                                            <span>34세</span>
-                                        </div>
-                                        <div>
-                                            <BuddyTemperature
-                                                isLabel={false}
-                                                isTempText={false}
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
+                        {buddies
+                            ? buddies.map((buddy: Buddy, index: number) => (
+                                  <div
+                                      key={index}
+                                      className="min-w-[200px] h-[75px] mx-1 rounded border border-gray-200 shadow-md"
+                                  >
+                                      <div className="flex items-center w-[120px] h-full">
+                                          <div className="flex-shrink-0 w-[75px] h-[75px]">
+                                              <MascotImage intent="happy" />
+                                          </div>
+                                          <div className="ml-2 flex flex-col">
+                                              <span className="text-xs font-bold text-gray-500 whitespace-nowrap">
+                                                  #
+                                                  {buddy.buddy_preferred_theme1}{' '}
+                                                  #
+                                                  {buddy.buddy_preferred_theme2}{' '}
+                                                  #
+                                                  {buddy.buddy_preferred_theme3}
+                                              </span>
+                                              <div className="text-m font-bold whitespace-nowrap">
+                                                  <span>
+                                                      {buddy.buddy_nickname}
+                                                  </span>
+                                                  <span> / </span>
+                                                  <span>
+                                                      {buddy.buddy_birth}세
+                                                  </span>
+                                              </div>
+                                              <div>
+                                                  <BuddyTemperature
+                                                      isLabel={false}
+                                                      isTempText={false}
+                                                  />
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              ))
+                            : null}
                     </div>
                 </div>
             )}
         </div>
     );
 }
+
 export default HomePageRecommnedBuddiesList;
