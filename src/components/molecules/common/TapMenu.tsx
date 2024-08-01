46213020b53e60ce27718ddd7ff6fcb48017496f
Eun Oh <bdohhhhh@gmail.com>
2024-08-02 08:43:21 +0900

    [style]: 탭 메뉴 좌우폭 맞춤

--- src/components/molecules/common/TapMenu.tsx
+++ src/components/molecules/common/TapMenu.tsx
@@ -31,7 +31,7 @@ const TapMenu: React.FC<TapMenuProps> = ({ pathname }) => {
     return (
         <div
             className={clsx(
-                'z-10 fixed bottom-0 left-1/2 transform -translate-x-1/2 max-w-[430px] min-w-[375px] bg-white border-t-2 border-gray-200 grid grid-cols-4 xl:hidden',
+                'z-10 fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full max-w-[430px] min-w-[375px] bg-white border-t-2 border-gray-200 grid grid-cols-4 xl:hidden',
                 hidden && 'hidden',
             )} // 변경 필요
         >
