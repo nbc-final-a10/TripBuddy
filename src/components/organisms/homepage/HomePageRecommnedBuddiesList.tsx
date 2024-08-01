8d0cc123081bdef300944e5d9913104658495d1c
Captain-Kim <brighthero@kakao.com>
2024-08-01 12:40:52 +0900

    fead: add home page 추천인기버디즈 리스트 렌더링 Link 컴포넌트로 버디프로필 연결

--- src/components/organisms/homepage/HomePageRecommnedBuddiesList.tsx
+++ src/components/organisms/homepage/HomePageRecommnedBuddiesList.tsx
@@ -3,9 +3,11 @@ import BuddyTemperature from '@/components/atoms/profile/BuddyTemperature';
 import SkeletonLoader from '@/components/atoms/profile/SkeletonLoader';
 import useTapScroll from '@/hooks/useTapScroll';
 import Image from 'next/image';
+import Link from 'next/link';
 import React, { useState, useEffect, useRef } from 'react';
 
 type Buddy = {
+    buddy_id: string;
     buddy_nickname: string;
     buddy_birth: number;
     buddy_temperature: number;
@@ -54,53 +56,57 @@ function HomePageRecommnedBuddiesList() {
                     >
                         {buddies
                             ? buddies.map((buddy: Buddy, index: number) => (
-                                  <div
+                                  <Link
                                       key={index}
-                                      className="min-w-[200px] h-[75px] mx-1 rounded border border-gray-200 shadow-md"
+                                      href={`/profile/${buddy.buddy_id}`}
+                                      passHref
                                   >
-                                      <div className="flex items-center w-full h-full">
-                                          <div className="flex-shrink-0 w-[75px] h-[75px]">
-                                              {buddy.buddy_profile_pic ? (
-                                                  <Image
-                                                      src={
-                                                          buddy.buddy_profile_pic
-                                                      }
-                                                      alt="profile"
-                                                      width={75}
-                                                      height={75}
-                                                  />
-                                              ) : (
-                                                  <MascotImage intent="happy" />
-                                              )}
-                                          </div>
-                                          <div className="mx-1 flex flex-col w-full">
-                                              <span className="text-xs font-bold text-gray-500 whitespace-nowrap overflow-hidden text-ellipsis">
-                                                  {buddy.buddy_preferred_buddy1 &&
-                                                  buddy.buddy_preferred_buddy2
-                                                      ? `#${buddy.buddy_preferred_buddy1} #${buddy.buddy_preferred_buddy2}`
-                                                      : '#태그없음'}
-                                              </span>
-                                              <div className="text-m font-bold whitespace-nowrap overflow-hidden text-ellipsis w-full">
-                                                  <span className="block truncate">
-                                                      {buddy.buddy_nickname}
-                                                  </span>
-                                                  {/* <span> / </span> */}
-                                                  {/* <span>
-                                                      {buddy.buddy_birth}세
-                                                  </span> */}
+                                      <div className="min-w-[200px] h-[75px] mx-1 rounded border border-gray-200 shadow-md cursor-pointer">
+                                          <div className="flex items-center w-full h-full">
+                                              <div className="flex-shrink-0 w-[75px] h-[75px]">
+                                                  {buddy.buddy_profile_pic ? (
+                                                      <Image
+                                                          src={
+                                                              buddy.buddy_profile_pic
+                                                          }
+                                                          alt="profile"
+                                                          width={75}
+                                                          height={75}
+                                                      />
+                                                  ) : (
+                                                      <MascotImage intent="happy" />
+                                                  )}
                                               </div>
-                                              <div className="w-full">
-                                                  <BuddyTemperature
-                                                      isLabel={false}
-                                                      isTempText={false}
-                                                      temperature={
-                                                          buddy.buddy_temperature
-                                                      }
-                                                  />
+                                              <div className="mx-1 flex flex-col w-full">
+                                                  <span className="text-xs font-bold text-gray-500 whitespace-nowrap overflow-hidden text-ellipsis">
+                                                      {buddy.buddy_preferred_buddy1 &&
+                                                      buddy.buddy_preferred_buddy2
+                                                          ? `#${buddy.buddy_preferred_buddy1} #${buddy.buddy_preferred_buddy2}`
+                                                          : '#태그없음'}
+                                                  </span>
+                                                  <div className="text-m font-bold whitespace-nowrap overflow-hidden text-ellipsis w-full">
+                                                      <span className="block truncate">
+                                                          {buddy.buddy_nickname}
+                                                      </span>
+                                                      {/* <span> / </span> */}
+                                                      {/* <span>
+                                                          {buddy.buddy_birth}세
+                                                      </span> */}
+                                                  </div>
+
+                                                  <div className="w-full">
+                                                      <BuddyTemperature
+                                                          isLabel={false}
+                                                          isTempText={false}
+                                                          temperature={
+                                                              buddy.buddy_temperature
+                                                          }
+                                                      />
+                                                  </div>
                                               </div>
                                           </div>
                                       </div>
-                                  </div>
+                                  </Link>
                               ))
                             : null}
                     </div>
