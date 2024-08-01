d4299054d4f11d94ff47c17813b67273add0bcdd
Eun Oh <bdohhhhh@gmail.com>
2024-08-01 14:34:38 +0900

    [update]: StoryList 에 내스토리 <> 몇시간전 표기 수정

--- src/components/organisms/stories/StoryList.tsx
+++ src/components/organisms/stories/StoryList.tsx
@@ -2,10 +2,12 @@
 
 import Loading from '@/app/(providers)/loading';
 import StoryCard from '@/components/molecules/stories/StoryCard';
+import { useAuth } from '@/hooks/auth';
 import { useStoryQuery } from '@/hooks/queries';
 import React from 'react';
 
 const StoryList: React.FC = () => {
+    const { buddy } = useAuth();
     const { data: story, isPending, error: storyError } = useStoryQuery();
 
     if (storyError) return <div>Error</div>;
@@ -22,7 +24,11 @@ const StoryList: React.FC = () => {
                         story.buddies.buddy_profile_pic || '/images/test.webp'
                     }
                     background_image={story.story_media}
-                    mode="my"
+                    mode={
+                        buddy?.buddy_id === story.buddies.buddy_id
+                            ? 'my'
+                            : 'story'
+                    }
                 />
             ))}
         </section>
