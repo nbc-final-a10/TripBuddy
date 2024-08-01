54e5f8f2358d776eea7776620abff6e2dbf31edd
slsl2 <kimminji7153@gmail.com>
2024-08-01 13:53:19 +0900

    feat: 채팅 목록 데이터 연결 작업 중

--- src/components/molecules/chatpage/ChatListItem.tsx
+++ src/components/molecules/chatpage/ChatListItem.tsx
@@ -1,28 +1,44 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 type ChatListItemProps = {
-    contractTripId: string;
-    chatName: string;
-    lastMessage: string;
+    contract_id: string;
+    contract_trip_id: string;
+    trip_title: string;
+    contract_buddies_profiles: string[];
 };
 
 const ChatListItem: React.FC<ChatListItemProps> = ({
-    contractTripId,
-    chatName,
-    lastMessage,
+    contract_id,
+    contract_trip_id,
+    trip_title,
+    contract_buddies_profiles,
 }) => {
+    // Render profile pictures as small circles
+    const renderProfilePictures = () => {
+        return contract_buddies_profiles.map((profilePic, index) => (
+            <div
+                key={index}
+                className="w-[30px] h-[30px] bg-gray-100 rounded-full overflow-hidden flex items-center justify-center border-2 border-white"
+                style={{ marginLeft: index === 0 ? '0' : '-10px' }}
+            >
+                <Image src={profilePic} alt="Profile" width={10} height={10} />
+            </div>
+        ));
+    };
+
     return (
-        <Link href={`/chat/${contractTripId}`}>
-            <div className="flex justify-between items-center">
-                <div className="flex space-x-1">
-                    <div className="w-[30px] h-[30px] bg-gray-100 rounded-full overflow-hidden"></div>
+        <Link href={`/chat/${contract_trip_id}`}>
+            <div className="flex justify-between items-center p-2 border-b border-gray-200 hover:bg-gray-50 transition">
+                <div className="flex items-center space-x-2">
+                    {renderProfilePictures()}
                 </div>
-                <div className="flex flex-col gap-1 justify-center flex-grow pl-2">
-                    <p className="text-sm font-bold">{chatName}</p>
-                    <p className="text-xs">{lastMessage}</p>
+                <div className="flex flex-col flex-grow pl-2">
+                    <p className="text-sm font-bold">{trip_title}</p>
+                    <p className="text-xs text-gray-500">last message</p>
                 </div>
-                <div className="text-xs flex flex-col gap-1">
+                <div className="text-xs flex flex-col items-end">
                     <span>19:02</span>
                     <span className="rounded-[16px] flex items-center justify-center bg-gray-300 p-1">
                         +3
--- src/components/organisms/chatpage/ChatList.tsx
+++ src/components/organisms/chatpage/ChatList.tsx
@@ -1,28 +1,154 @@
+'use client';
 import ChatListItem from '@/components/molecules/chatpage/ChatListItem';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import supabase from '@/utils/supabase/client';
+import { useAuth } from '@/hooks/auth';
 
-const chatData = [
-    {
-        contractTripId: '8a9313c8-0c0a-4ba4-804d-b0e101f39d9b',
-        chatName: '메시지 테스트!',
-        lastMessage: 'last message',
-    },
-    {
-        contractTripId: 'c8d9a501-5d13-4fb5-9e01-86cf98905d28',
-        chatName: '메시지 리스트 테스트',
-        lastMessage: 'last message',
-    },
-];
+type Contract = {
+    contract_id: string;
+    contract_trip_id: string;
+    contract_buddy_id: string;
+    isPending: boolean;
+    isValidate: boolean;
+};
 
-const ChatList: React.FC = () => {
+type BuddyProfile = {
+    buddy_id: string;
+    buddy_profile_pic: string;
+};
+
+type Trip = {
+    trip_id: string;
+    trip_title: string;
+};
+
+type ContractData = {
+    contract_id: string;
+    contract_trip_id: string;
+    trip_title: string;
+    contract_buddies_profiles: string[];
+};
+
+const ChatList = () => {
+    const { buddy: currentBuddy } = useAuth();
+    const [chatData, setChatData] = useState<ContractData[]>([]);
+
+    useEffect(() => {
+        const fetchContracts = async () => {
+            const { data: contracts, error: contractsError } = await supabase
+                .from<Contract>('contract')
+                .select('contract_id, contract_trip_id, contract_buddy_id')
+                .eq('contract_buddy_id', currentBuddy.buddy_id)
+                .eq('contract_isPending', false)
+                .eq('contract_isValidate', true);
+
+            if (contractsError) {
+                console.error('Error fetching contracts:', contractsError);
+                return;
+            } else console.log('contracts fetch 성공', contracts);
+
+            if (!contracts) return;
+
+            const contractIds = [
+                ...new Set(contracts.map(contract => contract.contract_id)),
+            ];
+            const tripIds = [
+                ...new Set(
+                    contracts.map(contract => contract.contract_trip_id),
+                ),
+            ];
+
+            const { data: allBuddies, error: allBuddiesError } = await supabase
+                .from<Contract>('contract')
+                .select('contract_buddy_id, contract_id')
+                .in('contract_id', contractIds);
+
+            if (allBuddiesError) {
+                console.error('Error fetching all buddies:', allBuddiesError);
+                return;
+            }
+
+            const allBuddyIds = allBuddies.map(
+                buddy => buddy.contract_buddy_id,
+            );
+            const uniqueBuddyIds = [...new Set(allBuddyIds)];
+
+            const { data: buddyProfiles, error: buddyError } = await supabase
+                .from<BuddyProfile>('buddies')
+                .select('buddy_id, buddy_profile_pic')
+                .in('buddy_id', uniqueBuddyIds);
+
+            if (buddyError) {
+                console.error('Error fetching buddy profiles:', buddyError);
+                return;
+            }
+
+            if (!buddyProfiles) return;
+
+            const buddyProfileMap = new Map<string, string>();
+            buddyProfiles.forEach(profile => {
+                buddyProfileMap.set(
+                    profile.buddy_id,
+                    profile.buddy_profile_pic,
+                );
+            });
+
+            const { data: trips, error: tripError } = await supabase
+                .from<Trip>('trips')
+                .select('trip_id, trip_title')
+                .in('trip_id', tripIds);
+
+            if (tripError) {
+                console.error('Error fetching trips:', tripError);
+                return;
+            }
+
+            if (!trips) return;
+
+            const tripTitleMap = new Map<string, string>();
+            trips.forEach(trip => {
+                tripTitleMap.set(trip.trip_id, trip.trip_title);
+            });
+
+            const contractDataMap = new Map<string, ContractData>();
+
+            contracts.forEach(contract => {
+                const { contract_id, contract_trip_id } = contract;
+
+                if (!contractDataMap.has(contract_id)) {
+                    contractDataMap.set(contract_id, {
+                        contract_id,
+                        contract_trip_id,
+                        trip_title: tripTitleMap.get(contract_trip_id) || '',
+                        contract_buddies_profiles: [],
+                    });
+                }
+
+                const buddyProfile =
+                    buddyProfileMap.get(contract.contract_buddy_id) || '';
+                const contractData = contractDataMap.get(contract_id);
+                if (contractData && buddyProfile) {
+                    contractData.contract_buddies_profiles.push(buddyProfile);
+                }
+            });
+
+            // Convert Map to array
+            const data = Array.from(contractDataMap.values());
+            setChatData(data);
+        };
+
+        fetchContracts();
+    }, [currentBuddy.buddy_id]);
+
     return (
         <div className="flex flex-col gap-5 p-4">
             {chatData.map(chat => (
                 <ChatListItem
-                    key={chat.contractTripId}
-                    contractTripId={chat.contractTripId}
-                    chatName={chat.chatName}
-                    lastMessage={chat.lastMessage}
+                    key={chat.contract_id}
+                    contract_id={chat.contract_id}
+                    contract_trip_id={chat.contract_trip_id}
+                    trip_title={chat.trip_title}
+                    contract_buddies_profiles={chat.contract_buddies_profiles}
                 />
             ))}
         </div>
