2485daca8ab2a789aa52cdb63fb8e270dd72564d
Captain-Kim <brighthero@kakao.com>
2024-08-02 00:57:05 +0900

    style: mypage 온도게이지 mx-8 부여

--- src/components/atoms/profile/BuddyTemperature.tsx
+++ src/components/atoms/profile/BuddyTemperature.tsx
@@ -10,7 +10,7 @@ const BuddyTemperature = ({
     isTempText = true,
 }: BuddyTemperatureProps) => {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col mx-8">
             {isLabel && (
                 <span className="block text-left xl:text-xl">버디즈 온도</span>
             )}
@@ -21,7 +21,7 @@ const BuddyTemperature = ({
             )}
             <div className="w-full rounded-full h-2 bg-[#A67000]">
                 <div
-                    className={`h-2 rounded-full bg-main-color`}
+                    className="h-2 rounded-full bg-main-color"
                     style={{ width: `${temperature}%` }}
                 ></div>
             </div>
