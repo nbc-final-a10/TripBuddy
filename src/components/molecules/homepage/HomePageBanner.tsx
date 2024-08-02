de1ede6709d0c986a77eb4a5b7c01f88be8af2bd
slsl2 <kimminji7153@gmail.com>
2024-08-02 12:08:34 +0900

    style: 배너 이미지 작업 중

--- src/components/molecules/homepage/HomePageBanner.tsx
+++ src/components/molecules/homepage/HomePageBanner.tsx
@@ -1,6 +1,16 @@
-const HomePageBanner = () => {
+import React from 'react';
+import Image from 'next/image';
+
+const HomePageBanner: React.FC = () => {
+    const bannerImgs = [
+        '/images/home_banner1/webp',
+        '/images/home_banner2/webp',
+        '/images/home_banner3/webp',
+    ];
+    const randomImg = bannerImgs[Math.floor(Math.random() * bannerImgs.length)];
+
     return (
-        <div className="text-left font-semibold text-2xl px-4 py-8 h-[200px] flex flex-col justify-end">
+        <div className="text-left text-white font-semibold text-2xl px-4 py-8 h-[200px] flex flex-col justify-end bg-[url('/images/home_banner1.webp')] bg-cover bg-center">
             <p>
                 <span className="font-bold text-3xl">여행자</span>님,
             </p>
