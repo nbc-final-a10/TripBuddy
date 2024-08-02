3d3f1e14cf937cdd9f204680cbca3ffc4ec09671
Eun Oh <bdohhhhh@gmail.com>
2024-08-02 11:37:22 +0900

    [update]: 데스크탑 헤더에프로필로가는 링크 작성

--- src/components/atoms/common/Header.tsx
+++ src/components/atoms/common/Header.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import MyPageIcon from '../../../../public/svg/mypageicon.svg';
 
 export default function Header() {
     return (
         <header className="hidden xl:flex relative w-[1080px] h-[100px] items-center bg-white justify-between z-50">
-            <div className="flex gap-12 items-center">
+            <div className="flex gap-12 items-center font-bold">
                 <Image
                     src="/images/logo.png"
                     alt="logo"
@@ -15,9 +16,12 @@ export default function Header() {
                 <Link href="/trips">여행 리스트</Link>
                 <Link href="/chat">채팅</Link>
             </div>
-            <div className="flex gap-12">
+            <div className="flex gap-12 items-center">
                 <Link href="/login">LOGIN</Link>
                 <Link href="/signup">JOIN</Link>
+                <Link href="/">
+                    <MyPageIcon />
+                </Link>
             </div>
         </header>
     );
