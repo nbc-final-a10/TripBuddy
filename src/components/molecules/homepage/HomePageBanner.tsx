00f184caa18b6d4e8b87ba64bf0c7bb4b30e5a92
slsl2 <kimminji7153@gmail.com>
2024-08-02 12:19:50 +0900

    style: 배너 랜덤 이미지로 변경

--- src/components/molecules/homepage/HomePageBanner.tsx
+++ src/components/molecules/homepage/HomePageBanner.tsx
@@ -1,11 +1,21 @@
 import Image from 'next/image';
 
 const HomePageBanner = () => {
+    const bannerImgs = [
+        'test_city.jpg',
+        'test_city2.jpg',
+        'test_banner1.webp',
+        'test_banner2.webp',
+        'test_banner3.webp',
+    ];
+    const randomImg = bannerImgs[Math.floor(Math.random() * bannerImgs.length)];
+    const randomImgSrc = `/images/${randomImg}`;
+
     return (
         <div className="relative h-[200px] z-0">
             <div className="relative text-left font-semibold text-2xl px-4 py-8 h-[230px] flex flex-col justify-end aspect-auto z-0">
                 <Image
-                    src="/images/test_city.jpg"
+                    src={randomImgSrc}
                     alt="banner"
                     fill
                     sizes="(max-width: 768px) 100vw, (max-width: 1280px) 100vw, 33vw"
