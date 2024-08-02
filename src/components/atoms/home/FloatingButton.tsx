3b1b1ae04bbcde37a99f9dacc3b7a58f1eeaf10b
Captain-Kim <brighthero@kakao.com>
2024-08-02 12:35:02 +0900

    fix: 비로그인 유저 모바일 홈페이지 플로팅 버튼 클릭 시 로그인 페이지로 리다이렉트 // showAlert z-50 부여하여 스타일 깨지는 오류 해결

--- src/components/atoms/home/FloatingButton.tsx
+++ src/components/atoms/home/FloatingButton.tsx
@@ -1,10 +1,25 @@
 'use client';
 
-import Link from 'next/link';
+import { useAuth } from '@/hooks/auth';
+import { showAlert } from '@/utils/ui/openCustomAlert';
+import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 
 export default function FloatingButton() {
     const [opacity, setOpacity] = useState(1);
+    const { buddy } = useAuth();
+    const router = useRouter();
+
+    const handleClick = () => {
+        if (buddy) {
+            router.push('/write');
+            return;
+        } else {
+            showAlert('caution', '로그인이 필요한 서비스입니다.', {
+                onConfirm: () => router.replace('/login'),
+            });
+        }
+    };
 
     useEffect(() => {
         const handleScroll = () => {
@@ -18,26 +33,25 @@ export default function FloatingButton() {
     }, []);
 
     return (
-        <Link href="/write">
-            <div
-                className="fixed bottom-20 right-8 bg-main-color text-white rounded-full p-4 shadow-lg"
-                style={{ opacity }}
+        <button
+            className="fixed bottom-20 right-8 bg-main-color text-white rounded-full p-4 shadow-lg"
+            style={{ opacity }}
+            onClick={handleClick}
+        >
+            <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
             >
-                <svg
-                    className="w-6 h-6"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M16 4l4 4-8 8H8v-4l8-8z"
-                    ></path>
-                </svg>
-            </div>
-        </Link>
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M16 4l4 4-8 8H8v-4l8-8z"
+                ></path>
+            </svg>
+        </button>
     );
 }
