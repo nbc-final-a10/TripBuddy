9ba2a2708e2a54947aba11eb9a39131957cf0e87
Captain-Kim <brighthero@kakao.com>
2024-08-04 00:59:46 +0900

    feat: add 마이페이지 내가 만든 여정 기능 구현

---
+++ src/app/api/trips/my/[id]/route.ts
@@ -0,0 +1,36 @@
+import { Trip } from '@/types/Trips.types';
+import { createClient } from '@/utils/supabase/server';
+import { PostgrestError } from '@supabase/supabase-js';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(
+    req: NextRequest,
+    { params }: { params: { id: string } },
+) {
+    const { id } = params;
+
+    const supabase = createClient();
+
+    const {
+        data: trip,
+        error: tripError,
+    }: {
+        data: Trip[] | null;
+
+        error: PostgrestError | null;
+    } = await supabase.from('trips').select('*').eq('trip_master_id', id);
+
+    if (tripError) {
+        console.error(tripError);
+        return NextResponse.json(
+            { error: tripError?.message },
+            { status: 401 },
+        );
+    }
+
+    if (!trip) {
+        return NextResponse.json({ error: 'Trip not found' }, { status: 404 });
+    }
+
+    return NextResponse.json(trip, { status: 200 });
+}
--- src/components/atoms/profile/MyTrips.tsx
+++ src/components/atoms/profile/MyTrips.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useRef, useState } from 'react';
 import { useAccordion, Accordion } from '@/hooks/useAccordion';
 import { BuddyProfileProps } from '@/types/ProfileParams.types';
 import { FaCalendarCheck, FaPen } from 'react-icons/fa';
+import TripCard from '@/components/molecules/trips/TripCard';
+import fetchWrapper from '@/utils/api/fetchWrapper';
+import { Trip } from '@/types/Trips.types';
+import useTapScroll from '@/hooks/useTapScroll';
 
 export default function MyTrips({ id }: BuddyProfileProps) {
     const participatingAccordion = useAccordion();
     const createdAccordion = useAccordion();
+    const [trips, setTrips] = useState<Trip[]>([]);
+    const createdTripsRef = useRef<HTMLDivElement>(null);
+    const participatingTripsRef = useRef<HTMLDivElement>(null);
+
+    const { createMouseDownHandler } = useTapScroll();
+
+    useEffect(() => {
+        const fetchTrips = async () => {
+            try {
+                const data = await fetchWrapper<Trip[]>(`/api/trips/my/${id}`, {
+                    method: 'GET',
+                });
+                setTrips(data);
+            } catch (error) {
+                console.error('Error fetching trips:', error);
+            }
+        };
+
+        fetchTrips();
+    }, [id]);
 
     return (
         <div className="p-4 bg-gray-100 rounded-xl">
@@ -15,7 +41,15 @@ export default function MyTrips({ id }: BuddyProfileProps) {
                 toggleAccordion={createdAccordion.toggleAccordion}
                 icon={<FaPen />}
             >
-                내가 만든 여정의 내용
+                <div
+                    className="overflow-x-scroll scrollbar-hidden flex gap-[10px]"
+                    ref={createdTripsRef}
+                    onMouseDown={createMouseDownHandler(createdTripsRef)}
+                >
+                    {trips.map(trip => (
+                        <TripCard key={trip.trip_id} trip={trip} mode="card" />
+                    ))}
+                </div>
             </Accordion>
             <Accordion
                 title="내가 참여한 여정"
