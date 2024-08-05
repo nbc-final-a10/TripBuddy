a445e6bdb672635bcee256d4a05530ecc750c81a
Eun Oh <bdohhhhh@gmail.com>
2024-08-05 20:02:23 +0900

    [update]: 스토리 순서 자기것이 제일 앞에 뜨게끔

--- src/components/molecules/homepage/HomePageStories.tsx
+++ src/components/molecules/homepage/HomePageStories.tsx
@@ -1,3 +1,4 @@
+'use client';
 import StoryCard from '../stories/StoryCard';
 import { StoryWithBuddies } from '@/types/Story.types';
 import { Buddy } from '@/types/Auth.types';
@@ -14,27 +15,37 @@ const HomePageStories: React.FC<HomePageStoriesProps> = ({
     buddy,
 }: HomePageStoriesProps) => {
     // stories에서 buddies.buddy_id 값이 같은 것들만 배열로 묶은 객체 생성
-    const sortedStories = useMemo(
-        () => groupStoriesByBuddyId(stories),
-        [stories],
-    );
+    const sortedStories = useMemo(() => {
+        const groupedStories = groupStoriesByBuddyId(stories);
+        const array = Object.entries(groupedStories).map(
+            ([buddyId, stories]) => ({
+                buddyId,
+                stories,
+            }),
+        );
+        const sortedArray = array.sort((a, b) => {
+            return a.buddyId === buddy?.buddy_id ? -1 : 1;
+        });
+
+        return sortedArray;
+    }, [stories, buddy]);
 
     return (
         <>
-            {Object.entries(sortedStories).map(([buddyId, stories]) => (
+            {sortedStories.map(story => (
                 <StoryCard
-                    key={buddyId}
-                    id={stories[0].story_id}
-                    buddy={stories[0].buddies}
-                    name={stories[0].buddies.buddy_nickname}
-                    created_at={stories[0].story_created_at}
+                    key={story.buddyId}
+                    id={story.stories[0].story_id}
+                    buddy={story.stories[0].buddies}
+                    name={story.stories[0].buddies.buddy_nickname}
+                    created_at={story.stories[0].story_created_at}
                     profile_image={
-                        stories[0].buddies.buddy_profile_pic ||
+                        story.stories[0].buddies.buddy_profile_pic ||
                         '/images/test.webp' // 추후변경요망
                     }
-                    background_image={stories[0].story_media}
+                    background_image={story.stories[0].story_media}
                     mode={
-                        buddy?.buddy_id === stories[0].buddies.buddy_id
+                        buddy?.buddy_id === story.stories[0].buddies.buddy_id
                             ? 'my'
                             : 'story'
                     }
