240a4d2ceae2dfbe38422d3adc126a3e755e7248
slsl2 <kimminji7153@gmail.com>
2024-08-08 17:49:47 +0900

    style: 알림 리스트 페이지 ui 구현

---
+++ src/app/(providers)/(authenticated)/notifications/page.tsx
@@ -0,0 +1,11 @@
+import NotificationsList from '@/components/organisms/notifications/NotificationsList';
+
+const NotificationsPage = () => {
+    return (
+        <>
+            <NotificationsList />
+        </>
+    );
+};
+
+export default NotificationsPage;
