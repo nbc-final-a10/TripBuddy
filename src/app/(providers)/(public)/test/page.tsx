156afe25da329924310c94585c1b2de06aacc97b
Eun Oh <bdohhhhh@gmail.com>
2024-08-08 23:06:31 +0900

    [add]: not-found 및 error 파일들 작성

--- src/app/(providers)/(public)/test/page.tsx
+++ src/app/(providers)/(public)/test/page.tsx
@@ -1,8 +1,11 @@
-import DefaultLoader from '@/components/atoms/common/defaultLoader';
+'use client';
+
 import PendingPage from '@/components/organisms/write/PendingPage';
 import React from 'react';
 
 function page() {
+    throw new Error('Test Error');
+
     return (
         <div>
             <div className="fixed flex items-center justify-center h-dvh w-dvw top-0 left-0 z-50" />
