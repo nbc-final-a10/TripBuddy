7694cd7fbe90ae053584ad0a51279d20fcf30267
slsl2 <kimminji7153@gmail.com>
2024-08-13 07:58:41 +0900

    style: home 페이지 전체보기 아이콘 추가, 타이틀 글씨 수정, margin-top 더 넓힘

--- src/components/molecules/homepage/HomePageTitle.tsx
+++ src/components/molecules/homepage/HomePageTitle.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
+import ArrowRight from '../../../../public/svg/ArrowRight.svg';
 
 interface HomePageTitleProps {
     title: string;
@@ -15,15 +16,23 @@ const HomePageTitle: React.FC<HomePageTitleProps> = ({
     href,
 }) => {
     return (
-        <div className="mb-4">
+        <div className="mb-4 mt-12">
             <div className="flex justify-between">
-                <p className="text-lg font-bold">{title}</p>
-                <Link href={href} className="text-xs">
-                    {buttonText}
+                <p className="text-grayscale-color-800 text-[22px] font-extrabold">
+                    {title}
+                </p>
+                <Link
+                    href={href}
+                    className="text-grayscale-color-700 text-[14px] font-medium flex justiy-end items-center"
+                >
+                    <span>{buttonText}</span>
+                    <ArrowRight />
                 </Link>
             </div>
 
-            <p className="text-sm">{description}</p>
+            <p className="text-grayscale-color-700 text-[16px] font-medium">
+                {description}
+            </p>
         </div>
     );
 };
