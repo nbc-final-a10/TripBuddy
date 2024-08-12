653749bc6a931c6b5247062909115a930af1ec8d
Eun Oh <bdohhhhh@gmail.com>
2024-08-12 17:26:46 +0900

    [chore]: hooks 폴더구조 변경 및 내보내기 일괄 수정2

--- src/components/organisms/homepage/HomePageContainer.tsx
+++ src/components/organisms/homepage/HomePageContainer.tsx
@@ -6,13 +6,12 @@ import HomePageTrips from '@/components/molecules/homepage/HomePageTrips';
 import React, { useEffect, useMemo, useRef } from 'react';
 import HomePageTitle from '@/components/molecules/homepage/HomePageTitle';
 import HomePageRecommendBuddiesList from './HomePageRecommendBuddiesList';
-import { useAuth } from '@/hooks/auth';
 import Navigate from '@/components/atoms/common/Navigate';
 import { showAlert } from '@/utils/ui/openCustomAlert';
 import filterOldTrips from '@/utils/trips/filterOldTrips';
 import { TripWithContract } from '@/types/Trips.types';
 import { useHomeQueries } from '@/hooks/queries';
-import { useTapScroll } from '@/hooks';
+import { useAuth, useTapScroll } from '@/hooks';
 
 const HomePageContainer = () => {
     const buddiesRef = useRef<HTMLDivElement>(null);
