95e72b7c73ec8ae12542a13d526aa15de0ece343
Captain-Kim <brighthero@kakao.com>
2024-08-13 21:29:49 +0900

    style: add 버디 프로필 온도 게이지 아이콘 렌더링

--- src/components/atoms/profile/BuddyTemperature.tsx
+++ src/components/atoms/profile/BuddyTemperature.tsx
@@ -12,15 +12,23 @@ const BuddyTemperature = ({
     isTempText = true,
 }: BuddyTemperatureProps) => {
     const smileIcons = [
-        '/svg/Settings.svg',
-        '/svg/Settings.svg',
-        '/svg/Settings.svg',
-        '/svg/Settings.svg',
-        '/svg/Settings.svg',
+        '/svg/smile1.svg',
+        '/svg/smile2.svg',
+        '/svg/smile3.svg',
+        '/svg/smile4.svg',
+        '/svg/smile5.svg',
     ];
 
     // 각 아이콘이 활성화될 임계 온도
     const thresholds = [20, 40, 60, 80, 100];
+
+    // 파란색으로 아이콘 바꾸는 로직 현재 온도와 가장 가까운 낮은 임계치를 찾아 그 인덱스를 반환
+    const closestThresholdIndex = thresholds.reduce(
+        (prevIndex, curr, index) => {
+            return temperature >= curr ? index : prevIndex;
+        },
+        0,
+    );
 
     return (
         <div className="flex flex-col w-full">
@@ -45,11 +53,16 @@ const BuddyTemperature = ({
                         height={8}
                         className="xl:w-4 xl:h-4"
                         style={{
-                            opacity: temperature >= thresholds[index] ? 1 : 0.3,
+                            // opacity: temperature >= thresholds[index] ? 1 : 0.3,
+                            filter:
+                                index === closestThresholdIndex
+                                    ? 'invert(31%) sepia(41%) saturate(7429%) hue-rotate(201deg) brightness(99%) contrast(106%)'
+                                    : 'none',
                         }}
                     />
                 ))}
             </div>
+            {/* 온도게이지 */}
             <div className="w-full rounded-full h-2 bg-[#A67000] mt-1">
                 <div
                     className="h-2 rounded-full bg-main-color"
