045e652f32307f9d5a76c28a6b9445260b07aaa6
Eun Oh <bdohhhhh@gmail.com>
2024-08-14 17:38:34 +0900

    [update]: 여정 상세 필터 모바일에서 다르게 구현

--- src/components/organisms/trips/TripListMobile.tsx
+++ src/components/organisms/trips/TripListMobile.tsx
@@ -7,9 +7,31 @@ import React, { useMemo, useState } from 'react';
 import TripCard from './TripCard';
 import { TripWithContract } from '@/types/Trips.types';
 import filterTripList from '@/utils/trips/filterTripList';
+import FilterIcon from '../../../../public/svg/Filter.svg';
+import { twMerge } from 'tailwind-merge';
+
+const FilterButton = [
+    {
+        title: '최신순',
+        value: 'latest',
+    },
+    {
+        title: '인기순',
+        value: 'bookmark',
+    },
+    {
+        title: '마감임박순',
+        value: 'imminent',
+    },
+    {
+        title: '마감여유순',
+        value: 'deadline',
+    },
+];
 
 const TripListMobile: React.FC = () => {
     const [filter, setFilter] = useState('latest');
+    const [filterOpen, setFilterOpen] = useState(false);
     const {
         data: tripsInfinite,
         isFetching,
@@ -22,33 +44,47 @@ const TripListMobile: React.FC = () => {
         [tripsInfinite, filter],
     );
 
-    const handleSelectChange = (
-        event: React.ChangeEvent<HTMLSelectElement>,
-    ) => {
-        const selectedValue = event.target.value;
-        setFilter(selectedValue);
-    };
+    const handleSelectChange =
+        (item: { title: string; value: string }) =>
+        (e: React.MouseEvent<HTMLButtonElement>) => {
+            const selectedValue = item.value;
+            if (selectedValue) setFilter(selectedValue);
+        };
 
     // 추후수정요망
     if (!tripsInfinite) return <div>No trips</div>;
     if (!filteredTrips) return <div>No filtered trips</div>;
 
     return (
-        <section className="w-full flex flex-col justify-center items-center h-auto pt-4">
-            <button className="flex justify-between items-center border-1 border-black rounded-md p-1">
-                <select
-                    className="w-full focus:outline-none"
-                    value={filter}
-                    onChange={handleSelectChange}
+        <section className="w-full flex flex-col justify-center items-center h-auto pt-2">
+            <div className="w-[90%] flex justify-between items-center">
+                <div className="flex w-full h-full justify-center items-center flex-1 gap-1">
+                    {filterOpen &&
+                        FilterButton.map(item => (
+                            <button
+                                key={item.value}
+                                className={twMerge(
+                                    'px-2 py-1 text-sm bg-white border border-grayscale-color-600 rounded-full shadow-sm text-grayscale-color-600',
+                                    item.value === filter
+                                        ? 'bg-primary-color-400 border border-primary-color-400 text-white'
+                                        : 'bg-white text-grayscale-color-600',
+                                )}
+                                onClick={handleSelectChange(item)}
+                            >
+                                {item.title}
+                            </button>
+                        ))}
+                </div>
+                <button
+                    className="flex justify-center items-center gap-0.5 rounded-full px-2 py-1 bg-primary-color-400 text-white shadow-sm"
+                    onClick={() => setFilterOpen(prev => !prev)}
                 >
-                    <option value="latest">최신순</option>
-                    <option value="bookmark">인기순</option>
-                    <option value="imminent">마감임박순</option>
-                    <option value="deadline">마감여유순</option>
-                </select>
-            </button>
+                    <span>필터</span>
+                    <FilterIcon />
+                </button>
+            </div>
 
-            <div className="grid w-full mx-auto place-items-center grid-cols-1 xl:grid-cols-4 gap-3 pb-28">
+            <div className="grid w-full mx-auto place-items-center grid-cols-1 xl:grid-cols-4 gap-3 pb-28 pt-2">
                 <InfiniteScroll
                     fetchNextPage={fetchNextPage}
                     hasNextPage={hasNextPage}
