3356a73af63fb4cfd71355a889add72764ec083a
Eun Oh <bdohhhhh@gmail.com>
2024-08-15 15:45:01 +0900

    [style]: 튜토리얼 반응형 스타일 수정

--- src/components/atoms/tutorial/Tuto.tsx
+++ src/components/atoms/tutorial/Tuto.tsx
@@ -1,8 +1,6 @@
-'use client';
-
-import React, { useState, useEffect } from 'react';
-import NextImage from 'next/image';
-import DefaultLoader from '../common/DefaultLoader';
+import React from 'react';
+// import DefaultLoader from '../common/DefaultLoader';
+import Image from 'next/image';
 
 type TutoProps = {
     step: number;
@@ -10,13 +8,13 @@ type TutoProps = {
 
 const content = [
     {
-        src: '/images/tuto0.webp',
+        src: '/images/tutorial_01.webp',
         alt: '첫 번째 튜토리얼 페이지에서 사용되는 이미지',
         title: <>각자의 성향대로 즐기는 여행!</>,
         description: <>트립버디즈가 나에 대해 알아가는 맞춤형 온보딩!</>,
     },
     {
-        src: '/images/tuto1.webp',
+        src: '/images/tutorial_02.webp',
         alt: '두 번째 튜토리얼 페이지에서 사용되는 이미지',
         title: (
             <>
@@ -28,13 +26,13 @@ const content = [
         description: <>평소 여행 스타일, 유형을 통한 맞춤형 추천 서비스!</>,
     },
     {
-        src: '/images/tuto2.webp',
+        src: '/images/tutorial_03.webp',
         alt: '세 번째 튜토리얼 페이지에서 사용되는 이미지',
         title: <>채팅을 통한 버디즈와 편리한 소통!</>,
         description: <>여정 계획, 맛집 공유 등 다양한 대화를 나눠봐요!</>,
     },
     {
-        src: '/images/tuto3.webp',
+        src: '/images/tutorial_04.webp',
         alt: '네 번째 튜토리얼 페이지에서 사용되는 이미지',
         title: (
             <>
@@ -46,7 +44,7 @@ const content = [
         description: <>나와 딱 맞는 버디즈를 직접 골라 여정을 함께해요!</>,
     },
     {
-        src: '/images/tuto4.webp',
+        src: '/images/tutorial_05.webp',
         alt: '다섯 번째 튜토리얼 페이지에서 사용되는 이미지',
         title: (
             <>
@@ -60,24 +58,24 @@ const content = [
 ];
 
 const Tuto: React.FC<TutoProps> = ({ step }) => {
-    const [isImageLoaded, setIsImageLoaded] = useState(false);
+    // const [isImageLoaded, setIsImageLoaded] = useState(false);
 
-    useEffect(() => {
-        setIsImageLoaded(false);
-        const img = new Image();
-        img.src = content[step].src;
-        img.onload = () => {
-            setIsImageLoaded(true);
-        };
-        img.onerror = () => {
-            setIsImageLoaded(false);
-        };
-    }, [step]);
+    // useEffect(() => {
+    //     setIsImageLoaded(false);
+    //     const img = new Image();
+    //     img.src = content[step].src;
+    //     img.onload = () => {
+    //         setIsImageLoaded(true);
+    //     };
+    //     img.onerror = () => {
+    //         setIsImageLoaded(false);
+    //     };
+    // }, [step]);
 
     return (
-        <div className=" bg-white mt-[8px]">
-            {!isImageLoaded && <DefaultLoader />}
-            <div className="h-[125px]">
+        <div className="relative h-full bg-white pt-[8px]">
+            {/* {!isImageLoaded && <DefaultLoader />} */}
+            <div className="relative h-[20%]">
                 <h1 className="text-center font-bold text-2xl md:text-3xl ">
                     {content[step].title}
                 </h1>
@@ -85,13 +83,14 @@ const Tuto: React.FC<TutoProps> = ({ step }) => {
                     {content[step].description}
                 </p>
             </div>
-            <div className="relative mt-[24px] mb-[24px] h-[500px] flex justify-center items-start">
+            <div className="relative flex justify-center items-start h-[80%]">
                 <div className="relative w-[324px] h-[426px] aspect-auto">
-                    <NextImage
+                    <Image
                         src={content[step].src}
                         alt={content[step].alt}
                         fill
                         priority
+                        loading="eager"
                         sizes="(max-width: 768px) 100%, (max-width: 1200px) 30%, 33%"
                         className="object-contain min-h-[324px] max-h-[426] w-[100%]  "
                     />
--- src/components/molecules/common/MainSectionWrapper.tsx
+++ src/components/molecules/common/MainSectionWrapper.tsx
@@ -27,7 +27,8 @@ const MainSectionWrapper = ({ children }: MainSectionWrapperProps) => {
         <main
             className={twMerge(
                 'bg-slate-50 xl:bg-slate-50 h-auto min-h-dvh overflow-hidden xl:min-h-[calc(100dvh-100px)] xl:h-auto',
-                pathname === '/tutorial' && 'bg-white xl:bg-white',
+                pathname === '/tutorial' &&
+                    'bg-white xl:bg-white xl:min-h-[calc(100dvh-100px)]',
                 pathname === '/stories' && 'xl:max-h-[calc(100dvh-100px)]',
                 pathname === '/trips' && 'xl:max-h-[calc(100dvh-100px)]',
             )}
@@ -35,6 +36,8 @@ const MainSectionWrapper = ({ children }: MainSectionWrapperProps) => {
             <section
                 className={twMerge(
                     'relative max-w-[430px] min-w-[320px] mx-auto min-h-dvh pb-[76px] xl:pb-0 xl:w-[1080px] xl:max-w-[1280px]',
+                    pathname === '/tutorial' &&
+                        'bg-white xl:bg-white xl:min-h-[calc(100dvh-100px)]',
                     isHidePaddingBottom && 'pb-0',
                     pathname.startsWith('/write') &&
                         'xl:min-h-[calc(100dvh-100px)] xl:h-[calc(100dvh-100px)]',
