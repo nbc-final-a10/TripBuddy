be99c113ad6bcdfefbb154eba7763aac6a03b660
Captain-Kim <brighthero@kakao.com>
2024-08-17 01:01:30 +0900

    style: useFollowListToggle 팔로잉-팔로워 누른 버튼 색상 변경

--- src/hooks/MyPage/useFollowListToggle.tsx
+++ src/hooks/MyPage/useFollowListToggle.tsx
@@ -19,16 +19,17 @@ export default function useFollowListToggle() {
 
     const FollowListToggleButton = () => {
         return (
-            <div className="flex justify-center mb-4">
-                <div className="w-full">
+            <div className="flex justify-center">
+                <div className="w-full mb-4">
+                    {/* TODO: border-b 숫자가 어떤 것도 안 먹힘 */}
                     <button
-                        className={`text-4xl px-4 py-2 ${activeButton === '팔로잉' ? 'border-b-2 border-main-color' : ''}`}
+                        className={`text-4xl font-bold px-4 py-2 ${activeButton === '팔로잉' ? 'border-b border-main-color text-main-color' : ''}`}
                         onClick={() => setActiveButton('팔로잉')}
                     >
                         팔로잉
                     </button>
                     <button
-                        className={`text-4xl px-4 py-2 ${activeButton === '팔로워' ? 'border-b-2 border-main-color' : ''}`}
+                        className={`text-4xl font-bold px-4 py-2 ${activeButton === '팔로워' ? 'border-b border-main-color text-main-color' : ''}`}
                         onClick={() => setActiveButton('팔로워')}
                     >
                         팔로워
