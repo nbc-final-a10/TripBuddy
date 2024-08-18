599f56769fb74e947422a660aac239979236ee21
Eun Oh <bdohhhhh@gmail.com>
2024-08-18 16:33:25 +0900

    [update]: 병준님 PR 머지 후 import export 오류 수정

--- src/app/(providers)/(conditional)/profile/follow/[id]/page.tsx
+++ src/app/(providers)/(conditional)/profile/follow/[id]/page.tsx
@@ -5,8 +5,8 @@ import { useParams } from 'next/navigation';
 import DefaultLoader from '@/components/atoms/common/DefaultLoader';
 import FollowingList from '@/components/molecules/profile/followList/FollowingList';
 import FollowerList from '@/components/molecules/profile/followList/FollowerList';
-import useFollowListToggle from '@/hooks/myPage/useFollowListToggle';
-import { useFollowCountQuery } from '@/hooks/queries/buddy/useGetFollowCounts';
+import useFollowListToggle from '@/hooks/MyPage/useFollowListToggle';
+import { useFollowCountQuery } from '@/hooks/queries';
 
 function FollowPage() {
     const { activeButton, FollowListToggleButton } = useFollowListToggle();
